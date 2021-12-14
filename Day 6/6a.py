2545e2aecbe261d0b35378b34804eb2b14f32cca
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2021-12-14 18:54:13 +0100

    Day 6 - Part Two

--- Day 6/6a.py
+++ Day 6/6a.py
@@ -7,11 +7,14 @@ NEW = 8
 path = pathlib.Path(__file__).parent.resolve().joinpath("input.txt")
 f = open(path, "r")
 
-fishes = list(map(lambda f: int(f), f.readline().split(",")))
+fishes = [int(age) for age in f.readline().split(',')]
+f.close()
 
+# The first approach one can think of, very suboptimal. Part 2 has the good one.
 for i in range(DAYS):
 	fishes = [f - 1 for f in fishes]							# Advance day
-	fishes.extend([NEW] * len([f for f in fishes if f == -1]))	# Reproduce
+	new_fishes = len([f for f in fishes if f == -1])			# Calculate reproduction
 	fishes = [RESET if f == -1 else f for f in fishes]			# Reset
+	fishes.extend([NEW] * new_fishes)										# Reproduce
 
 print(f'The day {DAYS} will be {len(fishes)} fishes.')
