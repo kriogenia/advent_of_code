122c2b4e6635e49f6116fab243ce48c837fe22fe
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2021-12-18 18:56:41 +0100

    Day 9. Part Two

--- Day 9/util.py
+++ Day 9/util.py
@@ -2,8 +2,27 @@ class Point:
 	value = 0
 	low_point = True
 
+	visited = False
+	neighbours = []
+	basin = []
+
 	def __init__(self, value: str):
 		self.value = int(value)
+		self.neighbours = []
+		self.basin = [self]
+
+	def add(self, point):
+		self.neighbours.append(point)
+
+	def travel(self):
+		if (self.value == 9 or self.visited):		# Stop condition to elude 9's
+			return []
+		self.visited = True
+		for point in self.neighbours:
+			if abs(self.value - point.value) == 1:
+				self.basin.extend(point.travel())
+		return self.basin
+
 
 def check_points(prev: Point, current: Point):
 	if prev.value >= current.value:
@@ -23,3 +42,11 @@ def line_to_points(line: str):
 	for char in line:
 		points.append(Point(char))
 	return points
+
+def graph_neighbours(matrix: "list(list(Point))"):
+	for line in matrix:
+		prev_point, rest = line[0], line[1:]
+		for point in rest:
+			point.add(prev_point)
+			prev_point.add(point)
+			prev_point = point
