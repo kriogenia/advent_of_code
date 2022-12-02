53ab217b1d0d8867c42f9acc161a5a430a3303be
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2022-12-02 01:55:38 +0100

    Day 01 - Part 02

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,25 +1,41 @@
-use std::str::FromStr;
+use std::{str::FromStr, collections::BinaryHeap};
 
-pub fn part_one(input: &str) -> Option<u32> {
-	let mut calories_counter = 0_u32;
-	let mut top_calories = 0;
+fn read_lines(input: &str, mut if_empty: impl FnMut(u32) -> ()) {
+	let mut calories_count = 0_u32;
 
 	for line in input.lines() {
 		if line.is_empty() {
-			if calories_counter > top_calories {
-				top_calories = calories_counter;
-			}
-			calories_counter = 0;
-			continue;
+			if_empty(calories_count);
+			calories_count = 0;
+		} else {
+			let calories: u32 = FromStr::from_str(line).expect("the text to be a 32bit integer");
+			calories_count += calories;
 		}
-		let calories: u32 = FromStr::from_str(line).expect("the text to be a 32bit integer");
-		calories_counter += calories;
 	}
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+	let mut top_calories = 0;
+
+	// simple solution with count and max comparison, memory efficient
+	read_lines(input, |calories_count| {
+		if calories_count > top_calories {
+			top_calories = calories_count;
+		}
+	});
+
 	return Some(top_calories);
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+	let mut top_calories = BinaryHeap::with_capacity(3);
+
+	// let's just use heaps, it's actually faster tho
+	read_lines(input, |calories_count| {
+		top_calories.push(calories_count);
+	});
+
+	Some(top_calories.into_iter().take(3).sum())
 }
 
 fn main() {
