dae8048d5f30ce0f456a068b9607c25e721351b4
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2022-12-02 01:20:35 +0100

    Day 01 - Part 01

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,5 +1,21 @@
+use std::str::FromStr;
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+	let mut calories_counter = 0_u32;
+	let mut top_calories = 0;
+
+	for line in input.lines() {
+		if line.is_empty() {
+			if calories_counter > top_calories {
+				top_calories = calories_counter;
+			}
+			calories_counter = 0;
+			continue;
+		}
+		let calories: u32 = FromStr::from_str(line).expect("the text to be a 32bit integer");
+		calories_counter += calories;
+	}
+	return Some(top_calories);
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
