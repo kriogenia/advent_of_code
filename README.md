ed37d4580ee751333ee664366598598531958719
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2022-12-08 20:54:11 +0100

    Day 08 - Part 01

--- README.md
+++ README.md
@@ -9,23 +9,25 @@ This repository uses a template of [fspoettel](https://github.com/fspoettel/adve
 
 The following times were recorded in my local machine. Parsing time is usually considered out of the equation but I will probably update the code to add that measuring too.
 
-|        | Part 1   | Part 2   |
-|--------|----------|----------|
-| Day 01 | 8.03µs   | 22.28µs  | 
-| Day 02 | 35.27µs  | 36.60µs  |
-| Day 03 | 208.27µs | 207.92µs |
-| Day 04 | 2.86µs   | 1.89µs   |
-| Day 05 | 23.96µs  | 64.81µs  |
-| Day 06 | 14.74µs  | 29.75µs  |
-| Day 07 | 37.72µs  | 32.90µs  |
+|        | Part 1   | Part 2    |
+|--------|----------|-----------|
+| Day 01 | 8.03µs   | 22.28µs   | 
+| Day 02 | 35.27µs  | 36.60µs   |
+| Day 03 | 208.27µs | 207.92µs  |
+| Day 04 | 2.86µs   | 1.89µs    |
+| Day 05 | 23.96µs  | 64.81µs   |
+| Day 06 | 14.74µs  | 29.75µs   |
+| Day 07 | 37.72µs  | 32.90µs   |
+| Day 08 | 31.50µs  | 225.73µs  |
 
-Total running time for all days and parts: 0.66ms (at the moment)
+Total running time for all days and parts: 0.67ms (at the moment)
 
 ## Some thoughs
 * The template of *fspoettel* is awesome but I tweaked a bit to match my intentions, like parsing the input just once for both parts if it's possible.
 * The ASCII inputs are awesome to work with bytes instead of strings and improve the performance times.
-* `Rc<RefCell<T>>` combo really asks for macros to stop those painfully long navigation lines.
+* `Rc<RefCell<T>>` combo really asks for macros and the use of `type` to stop those painfully long lines.
 * I love to craft specialized data structures like I had to do on **Day 06**.
 * I'm pretty proud of the decision to build a bigger array for the `Marker` data structure of **Day 06**. It allowed me to do the array advancement a lot better.
 * The **Day 06** I decided to use traits and a bit of duplicated code. I just wanted to use arrays and that requires sizes known at compilation time. I also wanted to make some solution based on default trait implementations for dedicated structs.
-* I was trying to make some weird things to solve **Day 07** instead of the logic approaches and that made me lose a lot of time. I was actually defeated. 
+* I was trying to make some weird things to solve **Day 07** instead of the logic approaches and that made me lose a lot of time. I was actually defeated the first day and had to left it to the next. It was easy with the obvious approach. 
+* For **Day 08** I looked the size of the grid manually to use arrays. I see it as part of the optimization, sorry not sorry. To deal with the difference between the example and input I used the `[cfg()]` macro and it worked wonders. Macros were also a big part of this day.
---
+++ src/bin/08.rs
@@ -0,0 +1,155 @@
+use advent_of_code::helpers::{AocResult, Folder};
+
+const DAY: u8 = 8;
+type Solution = Option<u32>;
+
+#[cfg(test)]
+const GRID_SIZE: usize = 5;
+#[cfg(not(test))]
+const GRID_SIZE: usize = 99; // handpicked size to work with arrays
+
+type Grid<T> = [[T; GRID_SIZE]; GRID_SIZE];
+const LAST_ELEMENT: usize = GRID_SIZE - 1;
+
+const MAX_HEIGHT: u8 = b'9';
+const HEIGHT_CHECK: u8 = MAX_HEIGHT * 2;
+
+struct Counter {
+    top_bottom: u8,
+    left_right: u8,
+    bottom_top: u8,
+    right_left: u8,
+}
+
+impl Counter {
+    fn new(heights: &Grid<u8>, offset: usize) -> Self {
+        Self {
+            top_bottom: heights[0][offset],
+            left_right: heights[offset][0],
+            bottom_top: heights[LAST_ELEMENT][offset],
+            right_left: heights[offset][LAST_ELEMENT],
+        }
+    }
+}
+
+macro_rules! visibility {
+    ($direction:ident -> $i:expr, $j:expr, $c: ident, $v:ident, $h:ident) => {
+        if $h[$i][$j] > $c.$direction {
+            $c.$direction = $h[$i][$j];
+            $v[$i][$j] = true
+        }
+    };
+}
+
+fn init_grids(input: String) -> (Grid<u8>, Grid<bool>) {
+    let mut height_grid = [[0; GRID_SIZE]; GRID_SIZE];
+    for (i, line) in input.lines().enumerate() {
+        for (j, height) in line.bytes().enumerate() {
+            height_grid[i][j] = height;
+        }
+    }
+
+    let mut visible_grid = [[false; GRID_SIZE]; GRID_SIZE];
+    for i in 0..GRID_SIZE {
+        visible_grid[0][i] = true;
+        visible_grid[i][0] = true;
+        visible_grid[LAST_ELEMENT][i] = true;
+        visible_grid[i][LAST_ELEMENT] = true;
+    }
+
+    (height_grid, visible_grid)
+}
+
+fn get_score(heights: &Grid<u8>, (i, j): (usize, usize)) -> u32 {
+    let height = heights[i][j];
+    let top = score_backwards(i, |h| heights[*h][j] < height);
+    let left = score_backwards(j, |h| heights[i][*h] < height);
+    let bottom = score_forwards(i, |h| heights[*h][j] < height);
+    let right = score_forwards(j, |h| heights[i][*h] < height);
+    top * left * bottom * right
+}
+
+fn score_backwards(position: usize, height_check: impl Fn(&usize) -> bool) -> u32 {
+    let to = position;
+    let count = (0..position).rev().take_while(height_check).count();
+    if count < to {
+        count as u32 + 1
+    } else {
+        count as u32
+    }
+}
+
+fn score_forwards(position: usize, height_check: impl Fn(&usize) -> bool) -> u32 {
+    let count = (position + 1..GRID_SIZE).take_while(height_check).count();
+    if count < (LAST_ELEMENT - position) {
+        count as u32 + 1
+    } else {
+        count as u32
+    }
+}
+
+pub fn part_one((heights, visibility): (&Grid<u8>, &mut Grid<bool>)) -> Solution {
+    for i in 1..LAST_ELEMENT {
+        let mut counter = Counter::new(heights, i);
+        for j in 1..LAST_ELEMENT {
+            visibility!(top_bottom -> j, i, counter, visibility, heights);
+            visibility!(left_right -> i, j, counter, visibility, heights);
+            if (counter.top_bottom + counter.left_right) >= HEIGHT_CHECK {
+                break;
+            }
+        }
+        for j in (1..LAST_ELEMENT).rev() {
+            visibility!(bottom_top -> j, i, counter, visibility, heights);
+            visibility!(right_left -> i, j, counter, visibility, heights);
+            if (counter.bottom_top + counter.right_left) >= HEIGHT_CHECK {
+                break;
+            }
+        }
+    }
+
+    let total = visibility
+        .iter_mut()
+        .flat_map(|x| x.iter_mut().filter(|x| **x))
+        .count() as u32;
+    Some(total)
+}
+
+pub fn part_two(heights: &Grid<u8>) -> Solution {
+    let mut max = 0;
+    for i in 1..LAST_ELEMENT {
+        for j in 1..LAST_ELEMENT {
+            let score = get_score(heights, (i, j));
+            if score > max {
+                max = score;
+            }
+        }
+    }
+    Some(max)
+}
+
+fn main() -> AocResult<()> {
+    let input = advent_of_code::read_file(Folder::Inputs, DAY);
+    let (heights, mut visibility) = init_grids(input);
+    advent_of_code::solve!(1, part_one, (&heights, &mut visibility));
+    advent_of_code::solve!(2, part_two, &heights);
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file(Folder::Examples, DAY);
+        let (heights, mut visibility) = init_grids(input);
+        assert_eq!(part_one((&heights, &mut visibility)), Some(21));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file(Folder::Examples, DAY);
+        let (heights, _) = init_grids(input);
+        assert_eq!(part_two(&heights), Some(8));
+    }
+}
