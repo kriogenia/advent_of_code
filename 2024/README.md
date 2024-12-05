61b156d909fa4fbb40fb69203cd63dfdd97585ee
SotoEstévez <ricardo@sotoestevez.dev>
2024-12-05 23:52:31 +0100

    2024_05_a

--- 2024/README.md
+++ 2024/README.md
@@ -23,8 +23,8 @@ zig build day01
 | [Day 1](https://adventofcode.com/2024/day/1) | ⭐ | ⭐ |
 | [Day 2](https://adventofcode.com/2024/day/2) | ⭐ | ⭐ |
 | [Day 3](https://adventofcode.com/2024/day/3) | ⭐ | ⭐ |
-| [Day 4](https://adventofcode.com/2024/day/4) | ⭐ |   |
-| [Day 5](https://adventofcode.com/2024/day/5) |   |   |
+| [Day 4](https://adventofcode.com/2024/day/4) | ⭐ | ⭐ |
+| [Day 5](https://adventofcode.com/2024/day/5) | ⭐ |   |
 | [Day 6](https://adventofcode.com/2024/day/6) |   |   |
 | [Day 7](https://adventofcode.com/2024/day/7) |   |   |
 | [Day 8](https://adventofcode.com/2024/day/8) |   |   |
--- 2024/src/day05.zig
+++ 2024/src/day05.zig
@@ -1,46 +1,82 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-
 const util = @import("util.zig");
 const gpa = util.gpa;
 
-const data = @embedFile("data/day05.txt");
+const List = std.ArrayList;
+const Map = std.AutoHashMap;
 
 pub fn main() !void {
-    
+    const input = try parse("input/day05.txt");
+    std.debug.print("ordered: {d}\n", .{try sumValidUpdateMedian(&input[0], &input[1])});
 }
 
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
-const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
+test "05" {
+    const input = try parse("examples/day05.txt");
+    try std.testing.expectEqual(143, try sumValidUpdateMedian(&input[0], &input[1]));
+}
 
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
+fn sumValidUpdateMedian(rules: *const Map(u8, List(u8)), updates: *const List([]u8)) !u32 {
+    var sum: u32 = 0;
+    for (updates.items) |update| {
+        if (try validateUpdate(rules, update)) {
+            sum += update[update.len / 2];
+        }
+    }
+    return sum;
+}
 
-const print = std.debug.print;
-const assert = std.debug.assert;
+fn validateUpdate(rules: *const Map(u8, List(u8)), update: []u8) !bool {
+    var validated = Map(u8, void).init(gpa);
+    for (update) |page| {
+        try validated.put(page, {});
+        const followers = rules.*.get(page) orelse continue;
+        for (followers.items) |follower| {
+            if (validated.contains(follower)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
+fn parse(filename: []const u8) !struct { Map(u8, List(u8)), List([]u8) } {
+    var rules = Map(u8, List(u8)).init(gpa);
+    var updates = List([]u8).init(gpa);
 
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
+    var lines = std.mem.splitScalar(u8, try util.readInputFile(filename), '\n');
+    while (lines.next()) |rule| {
+        if (rule.len == 0) break;
+
+        var pages = std.mem.splitScalar(u8, rule, '|');
+        const before = try parsePage(pages.next().?);
+        const after = try parsePage(pages.next().?);
+
+        const result = try rules.getOrPut(before);
+        if (result.found_existing) {
+            try result.value_ptr.*.append(after);
+        } else {
+            var list = List(u8).init(gpa);
+            try list.append(after);
+            result.value_ptr.* = list;
+        }
+    }
+
+    while (lines.next()) |update| {
+        if (update.len == 0) break;
+
+        var pages = std.mem.splitScalar(u8, update, ',');
+
+        var sequence = List(u8).init(gpa);
+        while (pages.next()) |page| {
+            try sequence.append(try parsePage(page));
+        }
+
+        try updates.append(try sequence.toOwnedSlice());
+    }
+
+    return .{ rules, updates };
+}
+
+fn parsePage(slice: []const u8) !u8 {
+    return std.fmt.parseInt(u8, slice, 10);
+}
