1b445d451a363fae758abd4fa09781560c3f660e
Soto Estévez <ricardo@sotoestevez.dev>
2024-12-13 18:54:20 +0100

    2024-07-b

--- 2024/src/day07.zig
+++ 2024/src/day07.zig
@@ -9,16 +9,21 @@ const parseInt = std.fmt.parseInt;
 const split = std.mem.splitScalar;
 
 pub fn main() !void {
-    std.debug.print("total calibration: {d}\n", .{try sumFeasible("input/day07.txt")});
+    const result = try sumFeasible("input/day07.txt");
+    std.debug.print("total calibration: {d}\n", .{result[0]});
+    std.debug.print("total calibration with concat: {d}\n", .{result[1]});
 }
 
 test "07" {
-    try std.testing.expectEqual(3749, try sumFeasible("examples/day07.txt"));
+    const result = try sumFeasible("examples/day07.txt");
+    try std.testing.expectEqual(3749, result[0]);
+    try std.testing.expectEqual(11387, result[1]);
 }
 
-fn sumFeasible(filename: []const u8) !u64 {
+fn sumFeasible(filename: []const u8) !struct { u64, u64 } {
     const input = try util.readInputFile(filename);
     var sum: u64 = 0;
+    var sumWithConcat: u64 = 0;
 
     var lines = split(u8, input, '\n');
     while (lines.next()) |line| {
@@ -36,14 +41,16 @@ fn sumFeasible(filename: []const u8) !u64 {
         var slice = try operands.toOwnedSlice();
         if (canMakeGoal(slice[1..], slice[0], left)) {
             sum += left;
+            sumWithConcat += left;
+        } else if (canMakeGoalWithConcat(slice[1..], slice[0], left)) {
+            sumWithConcat += left;
         }
     }
 
-    return sum;
+    return .{ sum, sumWithConcat };
 }
 
 fn canMakeGoal(operands: []u64, acc: u64, goal: u64) bool {
-    // std.debug.print("{d}: {d} + {any}\n", .{ goal, acc, operands });
     if (acc > goal) return false;
     if (operands.len == 0) return acc == goal;
 
@@ -56,4 +63,17 @@ test "canMakeGoal" {
     try operands.appendSlice(&[_]u64{ 6, 1, 5, 2, 15 });
     const slice = try operands.toOwnedSlice();
     try std.testing.expect(canMakeGoal(slice[1..], slice[0], 360));
+}
+
+fn canMakeGoalWithConcat(operands: []u64, acc: u64, goal: u64) bool {
+    if (acc > goal) return false;
+    if (operands.len == 0) return acc == goal;
+
+    const rest = operands[1..];
+    return canMakeGoalWithConcat(rest, acc * operands[0], goal) or canMakeGoalWithConcat(rest, acc + operands[0], goal) or canMakeGoalWithConcat(rest, concat(acc, operands[0]), goal);
+}
+
+fn concat(left: u64, right: u64) u64 {
+    const join = std.fmt.allocPrint(gpa, "{d}{d}", .{ left, right }) catch "";
+    return parseInt(u64, join, 10) catch 0;
 }
