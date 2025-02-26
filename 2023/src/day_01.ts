3a5def858df79275cdf0463d7187ce1e312d69ed
Soto Estévez <ricardo@sotoestevez.dev>
2025-02-26 17:22:20 +0100

    feat: 2023_01_a

--- 2023/src/day_01.ts
+++ 2023/src/day_01.ts
@@ -7,7 +7,19 @@ const parse: Parser<Input> = (gen: FileReader): Promise<Input> => {
 };
 
 const solveA: Solver<Input> = (input: Input) => {
-  return input.length;
+  return input
+    .map((line) => [...line])
+    .map((chars) => `${chars.find(isDigit)}${chars.reverse().find(isDigit)}`)
+    .map(Number)
+    .reduce((a, b) => a + b, 0);
+};
+
+const ZERO = "0".charCodeAt(0);
+const NINE = "9".charCodeAt(0);
+
+const isDigit = (c: string): boolean => {
+  const code = c.charCodeAt(0);
+  return code >= ZERO && code <= NINE;
 };
 
 const day: Day<Input> = {
--- 2023/src/main_test.ts
+++ 2023/src/main_test.ts
@@ -1,6 +1,8 @@
 import { assertEquals } from "@std/assert";
 import { Day, readFile } from "./common.ts";
 
+const SOLUTIONS = [undefined, 142];
+
 for (let i = 1; i < 2; i++) {
   Deno.test({
     name: `Day: ${i}`,
@@ -9,7 +11,7 @@ for (let i = 1; i < 2; i++) {
       const generator = readFile(`test/day_${number}.txt`);
       const day: Day<unknown> = (await import(`./day_${number}.ts`)).default;
       const result = day.a.parser(generator).then(day.a.solver);
-      assertEquals(await result, 4);
+      assertEquals(await result, SOLUTIONS[i]);
     },
   });
 }
